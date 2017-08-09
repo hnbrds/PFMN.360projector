fb887119920c853f03d4f68c7ee368737306a935
Jin Young Sung <JYSung@JYSung-MBP.local>
2017-08-09 15:32:42 +0900

    Spherical projection

--- SphericalPlayer.cpp
+++ SphericalPlayer.cpp
@@ -14,7 +14,6 @@
 #include <assert.h>
 
 #include "math/matrix4.h"
-#include "WaveFrontOBJ.h"
 #include "math/vector3.h"
 #include "cv/VideoFrame.h"
 
@@ -30,24 +29,86 @@ GLFWwindow* g_window = NULL;
 #define YAXIS vector3(0,1,0)
 #define ZAXIS vector3(0,0,1)
 
-using namepace std;
+using namespace std;
+using namespace cv;
 
 string video_dir;
 Player* player;
 
-double eye[1][9] =
+
+// Eye
+/*
+ * gluLookAt
+ * eyex, eyey, eyez : camera position
+ * centerx, centery, centerz : point camera is looking at
+ * upx, upy, upz : camera upward vector
+ */
+double eyes[7][9] =
 {
-    {28,18,18,0,2,0,0,1,0}
+    {0,0,0,0,0,1,0,1,0},
+    {0,0,0,-1,0,0,0,1,0},
+    {0,0,0,0,0,-1,0,1,0},
+    {0,0,0,1,0,0,0,1,0},
+    {0,0,0,0,1,0,0,0,1},
+    {0,0,0,0,-1,0,0,0,-1},
+    {120,50,180,0,0,0,0,1,0}
 };
-matrix4 wld2eye, eye2wld;
+int eyeCount = sizeof(eyes) / sizeof(eyes[0]);
+int eyeIndex;
+vector<matrix4> wld2eye, eye2wld;
+
+
+// Sphere
 matrix4 sphere2wld;
-WaveFrontOBJ* sphere;
 int sphereID;
-vector3 startPt;
+GLuint sphereText;
+float Radius = 70;
+
+typedef struct // struct for sphere
+{
+    double X;
+    double Y;
+    double Z;
+    
+    double U;
+    double V;
+}VERTICES;
+const int space = 5;
+const int vertexCount = (90/space)*(360/space)*4;
+VERTICES VERTEX[vertexCount];
+
 
 // Width and Height for video player window
 int width, height;
 static int isDrag = 0;
+
+
+/*********************************************************************************
+ * OpenCV Mat to OpenGL texture
+ **********************************************************************************/
+GLuint MatToTexture(Mat _image)
+{
+    Mat image;
+    _image.copyTo(image);
+    if(image.empty()) return -1;
+    cvtColor(image, image, CV_BGR2RGB);
+    flip(image, image, 1);
+    
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    
+    //Bind texture ID
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.cols, image.rows,
+                 0, GL_RGB, GL_UNSIGNED_BYTE, image.ptr());
+    
+    return textureID;
+}
+
 
 /*********************************************************************************
  * Draw x, y, z axis of current frame on screen.
@@ -69,21 +130,81 @@ void drawFrame(float len)
     glEnd();					// End drawing lines.
 }
 
+
 /*********************************************************************************
  * Draw sphere mesh
+ * Create sphere
  **********************************************************************************/
-void drawSphere(matrix4 const& _sphere2wld)
+void drawSphere(double R, GLuint texture)
 {
-    glPushMatrix();
-    glMultMatrixd(_sphere2wld.GLmatrix());
-    //drawFrame(5);
-    glEnable(GL_LIGHTING);
-    float frontColor[] = {1.0, 1.0, 1.0, 1.0};
-    //glMaterialfv(GL_FRONT, GL_AMBIENT, frontColor);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, frontColor);
+    //glScalef (0.0125 * R, 0.0125 * R, 0.0125 * R);
+    glRotatef (90, 1, 0, 0);
+    glBindTexture (GL_TEXTURE_2D, texture);
+    glBegin (GL_TRIANGLE_STRIP);
     
-    glCallList(sphereID);
-    glPopMatrix();
+    for (int i = 0; i <= vertexCount; i++){
+        glTexCoord2f (VERTEX[i].U, VERTEX[i].V);
+        glVertex3f (VERTEX[i].X, VERTEX[i].Y, -VERTEX[i].Z);
+    }
+    
+    for (int i = 0; i <= vertexCount; i++){
+        glTexCoord2f (VERTEX[i].U, -VERTEX[i].V);
+        glVertex3f (VERTEX[i].X, VERTEX[i].Y, VERTEX[i].Z);
+    }
+    glEnd();
+    
+}
+
+void createSphere(double R, double H, double K, double Z)
+{
+    int n=0;
+    
+    for (double j=0; j<= 90-space; j+=space){
+        for (double i=0; i<= 360-space; i+=space){
+            // Start editing our vertex.
+            // I am calculating the X value here.
+            VERTEX[n].X = R * sin((i) / 180 * M_PI) * sin((j) / 180 * M_PI) - H;
+            // The Y value here.
+            VERTEX[n].Y = R * cos((i) / 180 * M_PI) * sin((j) / 180 * M_PI) + K;
+            // The Z value here.
+            VERTEX[n].Z = R * cos((j) / 180 * M_PI) - Z;
+            // Now I am calculating the texture coordinates. I have used (2*j) as the texture
+            // is twice as wide as it is high. Hence 2:1. You can remove the (2*) if you wish
+            // to use i texture with the same width and height, or increase it accordingly.
+            VERTEX[n].V = (2 * j) / 360;
+            VERTEX[n].U = (i) / 360;
+            // Then start working with the next vertex
+            n++;
+
+            
+            // Then we do the same calculations as before, only adding the space variable
+            // to the j values.
+            VERTEX[n].X = R * sin((i) / 180 * M_PI) * sin((j + space) / 180 * M_PI) - H;
+            VERTEX[n].Y = R * cos((i) / 180 * M_PI) * sin((j + space) / 180 * M_PI) + K;
+            VERTEX[n].Z = R * cos((j + space) / 180 * M_PI) - Z;
+            VERTEX[n].V = (2 * (j + space)) / 360;
+            VERTEX[n].U = (i) / 360;
+            n++;
+            
+            // Then we do the same calculations as the first, only adding the space variable
+            // to the i values.
+            VERTEX[n].X = R * sin((i + space) / 180 * M_PI) * sin((j) / 180 * M_PI) - H;
+            VERTEX[n].Y = R * cos((i + space) / 180 * M_PI) * sin((j) / 180 * M_PI) + K;
+            VERTEX[n].Z = R * cos((j) / 180 * M_PI) - Z;
+            VERTEX[n].V = (2 * j) / 360;
+            VERTEX[n].U = (i + space) / 360;
+            n++;
+            
+            // Then we do the same calculations as the first again, only adding the space variable
+            // to both the j and the i values.
+            VERTEX[n].X = R * sin((i + space) / 180 * M_PI) * sin((j + space) / 180 * M_PI) - H;
+            VERTEX[n].Y = R * cos((i + space) / 180 * M_PI) * sin((j + space) / 180 * M_PI) + K;
+            VERTEX[n].Z = R * cos((j + space) / 180 * M_PI) - Z;
+            VERTEX[n].V = (2 * (j + space)) / 360;
+            VERTEX[n].U = (i + space) / 360;
+            n++;
+        }
+    }
 }
 
 
@@ -93,13 +214,25 @@ void drawSphere(matrix4 const& _sphere2wld)
  **********************************************************************************/
 void display()
 {
+    Mat frame = player-> getNextFrame();
+    
+    // Call OpenCV to get next frame
+    sphereText = MatToTexture(frame);
+    imshow("Sample Video", frame);
+    
     // call display() for every frame of the video
     glClearColor(0., 0.25f, 1.0f, 1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    // set view transformation
-    glLoadMatrixd(wld2eye.GLmatrix());
     
-    drawSphere(sphere2wld);
+    // set view transformation
+    glLoadMatrixd(wld2eye[eyeIndex].GLmatrix());
+    
+    //glMatrixMode(GL_MODELVIEW);
+    //glLoadIdentity();
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, sphereText);
+    
+    drawSphere(10, sphereText);
     glFlush();
 }
 
@@ -116,8 +249,10 @@ void reshape( int w, int h)
     glLoadIdentity();                       // Reset The Projection Matrix
     
     // Define perspective projection frustum
-    double aspect = width/double(height);
-    gluPerspective(45, aspect, 1, 1024);
+    double aspect = width/ double(height);
+    
+    // void gluPerpective(GLdouble fovy, GLdouble aspect, GLdouble near, GLdouble far);
+    gluPerspective(60, aspect, 1, 1024);
     glMatrixMode(GL_MODELVIEW);             // Select The Modelview Matrix
     glLoadIdentity();                       // Reset The Projection Matrix
 }
@@ -175,12 +310,6 @@ void onMouseDrag( int x, int y)
     }
 }
 
-void RenderScene(void)
-{
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glPushMatrix();
-}
-
 /*********************************************************************************
 * Call this part whenever user types keyboard.
 **********************************************************************************/
@@ -190,23 +319,30 @@ void onKeyPress(GLFWwindow *__win, int key, int __scancode, int action, int __mo
 void onKeyPress( int key, int action)
 #endif
 {
+    if(action == GLFW_RELEASE)
+        return ;
+    if ((key == ' ') || (key == 'r'))
+    {
+        printf("Viewpoint change\n", eyeIndex);
+        eyeIndex = (eyeIndex + 1) % eyeCount;
+    }
 }
     
 /*********************************************************************************
 * Load texure video from file
 * (Equirectangular video)
 **********************************************************************************/
-unsigned int loadTexture(const char* filename)
-{
-    return 1;
-}
-    
 void initialize()
 {
     // Set up OpenGL state
     glShadeModel(GL_SMOOTH);  // Smooth shading
     glEnable(GL_DEPTH_TEST);  // Enable depth testing
+    glEnable(GL_TEXTURE_2D);
     glDepthFunc(GL_LEQUAL);   // Type of depth test to do
+    
+    //glCullFace(GL_BACK);
+    //glFrontFace(GL_CCW);
+    //glEnable(GL_CULL_FACE);
     
     // Use perspective correct interpolation if available
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
@@ -224,47 +360,42 @@ void initialize()
     glEnable(GL_LIGHT0);
     
     { // Initialize sphere
-        sphere = new WaveFrontOBJ("sphere.obj");
-        
-        sphereID = glGenLists(1);               // Create display lists
-        glNewList(sphereID, GL_COMPILE);        // Begin compiliing the display list using sphereID
-        
-        sphere -> Draw();                       // Draw sphere on display list
-        glEndList();                            // Terminate compiling the display list. Draw sphere using 'sphereID'
-    
-        glPushMatrix();                         // Push current matrix of GL into stack
-        glLoadIdentity();                       // Set GL matrix Identity matrix
-        // Read modelview matrix about location and direction set above, store it in sphere2wld matrix
-        sphere2wld.getCurrentOpenGLmatrix(GL_MODELVIEW_MATRIX);
-        glPopMatrix();
+        createSphere(Radius,0,0,0);
     }
     
     { // Initialize eye transform
-        double* c = eye[0];
-        glPushMatrix();
-        glLoadIdentity();
-        gluLookAt(c[0], c[1], c[2], c[3], c[4], c[5], c[6], c[7], c[8]);
-        wld2eye.getCurrentOpenGLmatrix(GL_MODELVIEW_MATRIX);
-        glPopMatrix();
-        
-        matrix4 invmat;
-        invmat.inverse(wld2eye);
-        eye2wld = invmat;
+        for(int i = 0; i< eyeCount; i++)
+        {
+            double* eye = eyes[i];
+            wld2eye.push_back(matrix4());
+            glPushMatrix();
+            glLoadIdentity();
+            gluLookAt(eye[0], eye[1], eye[2], eye[3], eye[4], eye[5], eye[6], eye[7], eye[8]);
+            wld2eye[i].getCurrentOpenGLmatrix(GL_MODELVIEW_MATRIX);
+            glPopMatrix();
+            matrix4 invmat;
+            invmat.inverse(wld2eye[i]);
+            eye2wld.push_back(invmat);
+        }
     }
+    eyeIndex = 0;
 }
 
 int main(int argc, char* argv[])
 {
     if(argc != 2){
+        cout << "ERROR: Number of argument" << endl;
         return -1;
     }
     video_dir = argv[1];
+    player = new Player(video_dir);
     
-    width = 720;
-    height = 720;
+    width = 500;
+    height = 500;
     int BPP = 32;
     glfwInit(); // Init OpenGL
-    
+    namedWindow("Sample Video", 1);
+   
 #if GLFW_VERSION_MAJOR==3
     GLFWwindow* window = glfwCreateWindow(width, height, "360 Player", NULL, NULL);
     g_window=window;
@@ -284,7 +415,7 @@ int main(int argc, char* argv[])
         glfwPollEvents();
     }
 #else
-    // Create a window (8-bit depth-buffer, no alpha and stencil buffers, windowed)
+    // Create i window (8-bit depth-buffer, no alpha and stencil buffers, windowed)
    	glfwOpenWindow(width, height, BPP/4, BPP/4, BPP/4, 1, 8, 1, GLFW_WINDOW) ;
     glfwSetWindowTitle("360 Player");				// Make window whose name is "Simple Scene".
     int rv,gv,bv;
@@ -306,4 +437,5 @@ int main(int argc, char* argv[])
 #endif
     glfwTerminate();
     return 0;
+    
 }
--- cv/VideoFrame.cpp
+++ cv/VideoFrame.cpp
@@ -8,9 +8,7 @@ Player::Player(string const& _video_dir)
     video_dir = _video_dir;
     cap = new VideoCapture(video_dir);
     if(!cap->isOpened())
-    {
-        // ERROR msg
-    }
+        cout << "ERROR : not opened" << endl;
     
     frame_rate = cap->get(CV_CAP_PROP_FPS);
 }
@@ -23,32 +21,8 @@ cv::Mat Player::getNextFrame(void)
     return frame;
 }
 
+
 int Player::getFrameCount(void)
 {
     return cur_frame;
 }
-
-/*
-int main(int argc, char * argv[])
-{
-    Mat frame;
-    // Init videocapture
-    VideoCapture cap("../video/sample1.mp4");
-    if(!cap.isOpened())
-        return -1;
-    
-    double fps = cap.get(CV_CAP_PROP_FPS);
-    cout << fps << endl;
-    
-    //Mat edges;
-    namedWindow("Sample video", 1);
-    for(;;)
-    {
-        Mat frame;
-        cap >> frame;
-        imshow("Sample video", frame);
-        if(waitKey(30) == 27) break;
-    }
-    return 0;
-}
-*/
